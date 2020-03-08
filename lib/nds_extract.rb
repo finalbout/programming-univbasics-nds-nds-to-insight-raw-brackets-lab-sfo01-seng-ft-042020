da27a25b999682a374227f0a7ddd7358fef19bc2
John Tom Jr <johntjr.22@gmail.com>
2020-03-08 05:54:27 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -9,8 +9,11 @@ def directors_totals(nds)
 
   while i < nds.count do
     dir_name = nds[i][:name]
+    
     result[dir_name] = 0
+    
     movie = 0
+    
     while movie < nds[i][:movies].count do
       result[dir_name] += nds[i][:movies][movie][:worldwide_gross]
       movie +=1
